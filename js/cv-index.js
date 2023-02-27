7875c693941a5ed73cd8df0a18fffd93456ea615
Gonzalo-pasion <ferreyragonzalo727@gmail.com>
2023-02-27 00:26:20 -0300

    Primer escritura en el repositorio local

---
+++ js/cv-index.js
@@ -0,0 +1,8 @@
+function mostrar () {
+    document.getElementById('listas-ocultar').style.display = 'block';
+}
+
+function ocultar () {
+    document.getElementById('listas-ocultar').style.display = 'none';
+    
+}
